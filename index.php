4754bcc6f3bb1293bccfd31d1081f587a580d355
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-02-18 09:32:41 +0000

    First Assignment

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+$name = "Starboy";
+echo $name;
+echo "</br>";
+
+
+$age = 57;
+echo $age;
+echo "</br>";
+
+$salary = 56.90;
+echo $salary;
+echo "</br>";
+
+$bol = True;
+echo $bol;
+echo "</br>";
+
+$var = "Something";
+echo strlen($var);
+echo "</br>";
+
+$star1 = "Star";
+$star2 = "Boy";
+
+echo $star1 . " " .$star2;
+
+
+
+
+
+?>
